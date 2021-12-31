b669538d640ff9ffbee3cbcdda6e9694ead8145e
Ashikuzzaman <ashikuzzaman@tigerit.com>
2022-01-01 03:24:51 +0600

    Configured spring-data-r2dbc with h2 database and test

--- src/main/java/com/studevs/practice/practice01/Main.java
+++ src/main/java/com/studevs/practice/practice01/Main.java
@@ -1,10 +1,15 @@
 package com.studevs.practice.practice01;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -22,6 +27,29 @@ public class Main implements Serializable {
 
     @RequestMapping(value = "/")
     public String hello() {
+        log.debug("Calling: com.studevs.practice.practice01.Main.hello");
         return new Date().toString();
     }
+}
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/customer")
+class CustomerController {
+
+    private final CustomerRepository repository;
+
+    @GetMapping(value = "/get-all")
+    public Flux<Customer> getCustomers() {
+        log.info("Calling: com.studevs.practice.practice01.CustomerController.getCustomers");
+        return this.repository.findAll();
+    }
+}
+
+interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
+}
+
+
+record Customer(@Id Integer id, String name) {
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into customer(name) values('Name 1');
+insert into customer(name) values('Name 2');
+insert into customer(name) values('Name 3');
+insert into customer(name) values('Name 4');
+insert into customer(name) values('Name 5');
+insert into customer(name) values('Name 6');
+insert into customer(name) values('Name 7');
+insert into customer(name) values('Name 8');
+insert into customer(name) values('Name 9');
+insert into customer(name) values('Name 10');
