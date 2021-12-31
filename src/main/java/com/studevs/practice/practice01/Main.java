521e37216cdf7715da10a594435099876df35f51
Ashikuzzaman <ashikuzzaman@tigerit.com>
2022-01-01 04:29:28 +0600

    Configured postgresql db and initialize demo data

--- src/main/java/com/studevs/practice/practice01/Main.java
+++ src/main/java/com/studevs/practice/practice01/Main.java
@@ -1,18 +1,21 @@
 package com.studevs.practice.practice01;
 
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 @Slf4j
 @SpringBootApplication
@@ -41,15 +44,42 @@ class CustomerController {
     private final CustomerRepository repository;
 
     @GetMapping(value = "/get-all")
-    public Flux<Customer> getCustomers() {
+    public List<Customer> getCustomers() {
         log.info("Calling: com.studevs.practice.practice01.CustomerController.getCustomers");
         return this.repository.findAll();
     }
 }
 
-interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
+interface CustomerRepository extends JpaRepository<Customer, Integer> {
 }
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer")
+class Customer implements Serializable {
 
-record Customer(@Id Integer id, String name) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    private String name;
+}
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+class DataLoader implements ApplicationRunner {
+
+    private final CustomerRepository customerRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.debug("Calling: com.studevs.practice.practice01.DataLoader.run");
+        for (int i = 1; i <= 1000; i++) {
+            this.customerRepository.save(new Customer(null, "Customer " + i));
+        }
+    }
 }
