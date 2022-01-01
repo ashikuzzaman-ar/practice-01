6000661c5de044f2fbe770ffcae6face29aa8401
Ashikuzzaman <ashikuzzaman@tigerit.com>
2022-01-01 07:39:53 +0600

    Refactored and added polymorphic implementation for GraphQL schema generation

--- src/main/java/com/studevs/practice/practice01/Main.java
+++ src/main/java/com/studevs/practice/practice01/Main.java
@@ -2,18 +2,19 @@ package com.studevs.practice.practice01;
 
 import graphql.ExecutionResult;
 import graphql.GraphQL;
-import graphql.schema.GraphQLSchema;
 import graphql.schema.idl.RuntimeWiring;
 import graphql.schema.idl.SchemaGenerator;
 import graphql.schema.idl.SchemaParser;
-import graphql.schema.idl.TypeDefinitionRegistry;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.ResponseEntity;
@@ -61,39 +62,66 @@ class CustomerController {
     @PostMapping(value = "/get-all")
     public ResponseEntity<ExecutionResult> getCustomers(@RequestBody String query) {
         log.info("Calling: com.studevs.practice.practice01.CustomerController.getCustomers");
-        return ResponseEntity.ok(this.customerService.getGraphQL().execute(query));
+        return ResponseEntity.ok(this.customerService.execute(query));
     }
 }
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
-class CustomerService implements Serializable {
-
-    @Value("classpath:graphql/schema.graphql")
-    private Resource resource;
+class CustomerService extends GraphQLServiceCommon {
 
     @Getter
-    private GraphQL graphQL;
+    @Value("classpath:graphql/customer.graphql")
+    private Resource resource;
 
     private final CustomerRepository customerRepository;
 
+    protected RuntimeWiring buildRuntimeWiring() {
+        log.debug("Calling: com.studevs.practice.practice01.CustomerService.buildRuntimeWiring");
+        return RuntimeWiring.newRuntimeWiring().type("Query", typeWiring -> typeWiring.dataFetcher("customers", dataFetchingEnvironment -> this.customerRepository.findAll())).build();
+    }
+}
+
+@Slf4j
+@Service
+abstract class GraphQLServiceCommon implements Serializable {
+    protected abstract Resource getResource();
+
+    protected abstract RuntimeWiring buildRuntimeWiring();
+
+    @Autowired
+    private SchemaParser schemaParser;
+
+    @Autowired
+    private SchemaGenerator schemaGenerator;
+
+    private GraphQL graphQL;
+
     @PostConstruct
     private void loadSchema() throws IOException {
-        log.debug("Calling: com.studevs.practice.practice01.CustomerService.loadSchema");
-        File schemaFile = this.resource.getFile();
-        TypeDefinitionRegistry typeDefinitionRegistry = new SchemaParser().parse(schemaFile);
-        RuntimeWiring runtimeWiring = buildWiring();
-        GraphQLSchema graphQLSchema = new SchemaGenerator().makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
-        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+        log.debug("Calling: com.studevs.practice.practice01.GraphQLService.loadSchema");
+        File schemaFile = this.getResource().getFile();
+        log.debug("Generating schema for: {}", schemaFile.getName());
+        this.graphQL = GraphQL.newGraphQL(this.schemaGenerator.makeExecutableSchema(this.schemaParser.parse(schemaFile), this.buildRuntimeWiring())).build();
     }
 
-    private RuntimeWiring buildWiring() {
-        log.debug("Calling: com.studevs.practice.practice01.CustomerService.buildWiring");
-        return RuntimeWiring.newRuntimeWiring()
-                .type("Query", typeWiring -> typeWiring
-                        .dataFetcher("customers", dataFetchingEnvironment -> this.customerRepository.findAll()))
-                .build();
+    public ExecutionResult execute(final String query) {
+        return this.graphQL.execute(query);
+    }
+}
+
+@Configuration
+class GraphQLConfiguration implements Serializable {
+
+    @Bean
+    public SchemaParser getSchemaParser() {
+        return new SchemaParser();
+    }
+
+    @Bean
+    public SchemaGenerator getSchemaGenerator() {
+        return new SchemaGenerator();
     }
 }
 
