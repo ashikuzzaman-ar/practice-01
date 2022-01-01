21e8bad6d957d05a5b9a88addfc531845fe8964e
Ashikuzzaman <ashikuzzaman@tigerit.com>
2022-01-01 06:50:46 +0600

    Implemented basic graphql endpoint

--- src/main/java/com/studevs/practice/practice01/Main.java
+++ src/main/java/com/studevs/practice/practice01/Main.java
@@ -1,21 +1,36 @@
 package com.studevs.practice.practice01;
 
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.*;
+import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
+import java.util.Random;
 
 @Slf4j
 @SpringBootApplication
@@ -41,12 +56,44 @@ public class Main implements Serializable {
 @RequestMapping(value = "/customer")
 class CustomerController {
 
-    private final CustomerRepository repository;
+    private final CustomerService customerService;
 
-    @GetMapping(value = "/get-all")
-    public List<Customer> getCustomers() {
+    @PostMapping(value = "/get-all")
+    public ResponseEntity<ExecutionResult> getCustomers(@RequestBody String query) {
         log.info("Calling: com.studevs.practice.practice01.CustomerController.getCustomers");
-        return this.repository.findAll();
+        return ResponseEntity.ok(this.customerService.getGraphQL().execute(query));
+    }
+}
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+class CustomerService implements Serializable {
+
+    @Value("classpath:graphql/schema.graphql")
+    private Resource resource;
+
+    @Getter
+    private GraphQL graphQL;
+
+    private final CustomerRepository customerRepository;
+
+    @PostConstruct
+    private void loadSchema() throws IOException {
+        log.debug("Calling: com.studevs.practice.practice01.CustomerService.loadSchema");
+        File schemaFile = this.resource.getFile();
+        TypeDefinitionRegistry typeDefinitionRegistry = new SchemaParser().parse(schemaFile);
+        RuntimeWiring runtimeWiring = buildWiring();
+        GraphQLSchema graphQLSchema = new SchemaGenerator().makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+        this.graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+    }
+
+    private RuntimeWiring buildWiring() {
+        log.debug("Calling: com.studevs.practice.practice01.CustomerService.buildWiring");
+        return RuntimeWiring.newRuntimeWiring()
+                .type("Query", typeWiring -> typeWiring
+                        .dataFetcher("customers", dataFetchingEnvironment -> this.customerRepository.findAll()))
+                .build();
     }
 }
 
@@ -55,6 +102,7 @@ interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -66,6 +114,8 @@ class Customer implements Serializable {
     private Integer id;
 
     private String name;
+
+    private Integer age;
 }
 
 @Slf4j
@@ -78,8 +128,9 @@ class DataLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.debug("Calling: com.studevs.practice.practice01.DataLoader.run");
-        for (int i = 1; i <= 1000; i++) {
-            this.customerRepository.save(new Customer(null, "Customer " + i));
+        Random random = new Random();
+        for (int i = 1; i <= 10; i++) {
+            log.debug("{}", this.customerRepository.save(new Customer(null, "Customer " + i, random.nextInt(100))));
         }
     }
 }
