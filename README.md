a2c14c108accea6289312d395f79930926d09bb3
Ashikuzzaman <ashikuzzaman@tigerit.com>
2022-01-04 22:54:09 +0600

    Added context-path property to application.yml

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Practice project with spring boot, postgresql etc
 
-URL[`POST`]: `http://localhost:8888/customer/get-all`
+URL[`POST`]: `http://localhost:8888/practice-01/api/customer/get-all`
 
 ```graphql
 {
--- src/main/java/com/studevs/practice/practice01/Main.java
+++ src/main/java/com/studevs/practice/practice01/Main.java
@@ -3,7 +3,7 @@ package com.studevs.practice.practice01;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
@@ -20,7 +20,7 @@ public class Main implements Serializable {
         log.info("Started application");
     }
 
-    @RequestMapping(value = "/")
+    @GetMapping(value = "/")
     public String hello() {
         log.debug("Calling: com.studevs.practice.practice01.Main.hello");
         return new Date().toString();
